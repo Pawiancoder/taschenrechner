2ad0c92bbebee73bb3af68e41ab4b8e339c54887
Pawiancoder <110484323+Pawiancoder@users.noreply.github.com>
2022-12-19 17:52:21 +0100

    Zahlen auswählen

--- main.ts
+++ main.ts
@@ -5,9 +5,12 @@ let number1 = 0;
 let op = "";
 let number2 = 0;
 let result;
+let chooseOp = false;
+const operator = ["+","-","x",":"];
 
 basic.forever(function () {
-    if (calculating == false) {
+    
+    if (chooseOp == true) {
         basic.showLeds(`
         . . . . .
         . . # . .
@@ -36,13 +39,13 @@ basic.forever(function () {
         . . # . .
         . . . . .
         `)
+        
     }
 });
 
 
 input.onButtonPressed(Button.A, () => {
-    calculating = true;
-    basic.clearScreen();
+    numbers1();
 })
 
 number1 = 1;
@@ -55,3 +58,51 @@ const calc = () => {
     }
 }
 
+const numbers1 = () => {
+    calculating = true;
+    basic.clearScreen();
+    basic.showString("1");
+    
+    
+    input.onButtonPressed(Button.A, () => {
+        number1 = number1 + 1;
+        basic.showNumber(number1);
+    })
+
+    input.onButtonPressed(Button.B, () => {
+        number1 = number1 -1;
+        basic.showNumber(number1);
+    })
+
+    input.onButtonPressed(Button.AB, () => {
+        number1 = number1;
+        basic.showIcon(IconNames.Yes);
+
+    input.onButtonPressed(Button.A, () => {
+        numbers2();
+    })
+
+    })
+};
+
+const numbers2 = () => {
+    //Code um nummer 2 zu erstllen
+    basic.showNumber(0);
+    number2 = 0;
+    input.onButtonPressed(Button.A, () => {
+        number2 = number2 + 1;
+        basic.showNumber(number2);
+    })
+    input.onButtonPressed(Button.B, () => {
+        number2 = number2 - 1;
+        basic.showNumber(number2);
+    })
+    input.onButtonPressed(Button.AB, () => {
+        number2 = number2;
+        basic.showIcon(IconNames.Yes);
+    })
+}
+
+const zeichen = () => {
+    chooseOp = true;
+}
