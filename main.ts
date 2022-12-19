572d36a5941df3b0c46e4930f4be985842b31356
Pawiancoder <110484323+Pawiancoder@users.noreply.github.com>
2022-12-19 15:26:21 +0100

    Ladeszene (+  x : )

--- main.ts
+++ main.ts
@@ -1,6 +1,30 @@
 
 //Taschenrechner
+let calculating = false;
 let number1 = 0;
 let op = "";
+let number2 = 0;
+let result;
 
+basic.forever(function () {
+    if (calculating == false) {
+        basic.showString("+ - x :");
+    }
+});
+
+
+input.onButtonPressed(Button.A, () => {
+    calculating = true;
+    basic.clearScreen();
+})
+
+number1 = 1;
+number2 = 4;
+op = ":";
+
+const calc = () => {
+    if (op == ":") {
+
+    }
+}
 
